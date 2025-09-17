d685c02c3fa550c52d77292d6fefddb347abbe9c
wallen0322 <496727593@qq.com>
2025-09-18 04:12:04 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,13 +2,16 @@ from .qi_edit import (
     QI_TextEncodeQwenImageEdit_Safe,
     QI_VAEDecodeHQ,
 )
+from .qi_refedit import QI_RefEditEncode_Safe
 
 NODE_CLASS_MAPPINGS = {
     "QI_TextEncodeQwenImageEdit_Safe": QI_TextEncodeQwenImageEdit_Safe,
     "QI_VAEDecodeHQ":                  QI_VAEDecodeHQ,
+    "QI_RefEditEncode_Safe":           QI_RefEditEncode_Safe,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "QI_TextEncodeQwenImageEdit_Safe": "QI • TextEncodeQwenImageEdit — by wallen0322",
     "QI_VAEDecodeHQ":                  "QI • VAE Decode — by wallen0322",
+    "QI_RefEditEncode_Safe":           "QI • RefEdit Encode — by wallen0322",
 }
