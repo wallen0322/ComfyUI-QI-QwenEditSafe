834fd283332b0973d643a7e8a5b57714456456e5
wallen0322 <496727593@qq.com>
2025-09-18 17:06:52 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,12 +6,12 @@ from .qi_refedit import QI_RefEditEncode_Safe
 
 NODE_CLASS_MAPPINGS = {
     "QI_TextEncodeQwenImageEdit_Safe": QI_TextEncodeQwenImageEdit_Safe,
-    "QI_VAEDecodeHQ":                  QI_VAEDecodeHQ,
-    "QI_RefEditEncode_Safe":           QI_RefEditEncode_Safe,
+    "QI_VAEDecodeHQ": QI_VAEDecodeHQ,
+    "QI_RefEditEncode_Safe": QI_RefEditEncode_Safe,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "QI_TextEncodeQwenImageEdit_Safe": "QI • TextEncodeQwenImageEdit — by wallen0322",
-    "QI_VAEDecodeHQ":                  "QI • VAE Decode — by wallen0322",
-    "QI_RefEditEncode_Safe":           "QI • RefEdit Encode — by wallen0322",
+    "QI_TextEncodeQwenImageEdit_Safe": "Qwen图像编辑编码器 — by wallen0322",
+    "QI_VAEDecodeHQ": "Qwen高质量VAE解码器 — by wallen0322",
+    "QI_RefEditEncode_Safe": "Qwen一致性编辑编码器 — by wallen0322",
 }
